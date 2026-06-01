24bc634b7bcc48d997873a31c94ada82d91a0933
ParkJiHwan1 <ajtwls010156@gmail.com>
2026-06-01 10:59:02 +0900

    백엔드 서버입니다.

---
+++ howmuch_backend/src/main/java/com/howmuch_team/howmuch_backend/HowmuchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.howmuch_team.howmuch_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowmuchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowmuchBackendApplication.class, args);
+	}
+
+}
---
+++ howmuch_backend/src/test/java/com/howmuch_team/howmuch_backend/HowmuchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howmuch_team.howmuch_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowmuchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
